c9355814a324e7a47a454115275f4a3a6fe3bbe9
Lara Lesar <lara.m.lesar@gmail.com>
2020-12-02 13:21:50 -0600

    header sourced and working

---
+++ src/components/DinnerSupplies/DinnerSupplies.js
@@ -0,0 +1,16 @@
+// imports
+// import { Component } from 'react';
+
+// // class
+// class DinnerSupplies extends Component{
+//     render(){
+//         return(
+//             <div>
+//                 <h2>hello from DinnerSupplies</h2>
+//             </div>
+//         ) // end return
+//     } // end render
+// } // end class
+
+// // export 
+// export default DinnerSupplies;
---
+++ src/components/Header/Header.js
@@ -0,0 +1,17 @@
+// imports
+import React, { Component } from 'react';
+
+// class
+class Header extends Component{
+    render(){
+        return(
+        <header>
+          <h1>Prim Proper Props</h1>
+        </header>
+            
+        ) // end return
+    } // end render
+} // end class
+
+// export 
+export default Header;
